e9091cb3c7a1c3895cc0096d80207b05b5e94bdc
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-07-07 14:10:44 +0200

    Added command line arguments etc.

--- src/napp.py
+++ src/napp.py
@@ -1,16 +1,36 @@
 from converter import Converter
+import argparse
 
 # AGE: format to 3 positions
 mappings = {
     'AGE': lambda x: x.zfill(3)
 }
 
-nocode = ['OCCSTRNG', 'HISPAN']
+nocode = ['BPLPARSE', 'CFU', 'CFUSIZE', 'CITYPOP', 'CNTYAREA', 'COUNTYUS',
+          'ELDCH', 'ENUMDIST', 'FAMUNIT', 'HEADLOC', 'HHNBRNO', 'HHWT',
+          'LINENUM', 'MOMLOC', 'NAMEFRST', 'NAMELAST', 'NFAMS', 'NHGISJOIN',
+          'OCCSTRNG', 'OCSCORUS', 'PAGENUM', 'PARISHGB', 'PARSE', 'PERNUM',
+          'PERWT', 'POPLOC', 'PRMFAMSZ', 'QOCCGB', 'REALPROP', 'RECTYPE',
+          'REEL', 'RELATS', 'RESLSNO', 'SDSTCA', 'SEAUS', 'SEIUS', 'SERIAL',
+          'SPLOC', 'YNGCH', 'YRSUSA1', 'NUMPERHH']
+
+
+parser = argparse.ArgumentParser(description="Convert NAPP files")
+parser.add_argument('source_file', metavar='source_file', type=str)
+parser.add_argument('target_file', metavar='target_file', type=str)
+parser.add_argument('dataset_name', metavar='dataset_name', type=str)
+parser.add_argument('--stop', metavar='N', type=int, default=None, required=False)
+
+
 
 
 if __name__ == '__main__':
-    infile = 'data/napp_test_data.csv'
-    outfile = 'data/napp_test_data.ttl'
+    args = parser.parse_args()
+
+    infile = args.source_file
+    outfile = args.target_file
+    dataset_name = args.dataset_name
+    stop = args.stop
     c = Converter(mappings=mappings, nocode=nocode, family='napp')
 
-    c.convert(infile, outfile, 'test_data')
+    c.convert(infile, outfile, dataset_name=dataset_name, stop=stop)
