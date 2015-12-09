8d5d0f76f4168f93c3753f32f2843e9362db4079
kathrinrin <k.dentler@vu.nl>
2015-12-09 16:41:16 +0100

    new way to load clio data

--- scripts/clear_clio_graph.sql
+++ scripts/clear_clio_graph.sql
@@ -1,1 +1,1 @@
-SPARQL CLEAR GRAPH <http://data.socialhistory.org/resource/gdppc/5343176a06654392f04b0dcf163d1f1f0f65ffce>;
+SPARQL CLEAR GRAPH <http://data.socialhistory.org/resource/gdppc/5343176a06654392f04b0dcf163d1f1f0f65ffce/>;
--- scripts/load_clio_data.sql
+++ scripts/load_clio_data.sql
@@ -1,16 +1,1 @@
-
--- Disable auto indexing before doing bulk insertions / deletions
-
--- Deleting previous entries of loader script
-delete from DB.DBA.load_list;
-
---      <folder with data>  <pattern>    <default graph if no graph file specified>
-ld_dir ('/scratch/clariah-sdh/converters/src/clio_converter/rdf', '*.ttl', 'http://data.socialhistory.org/resource/gdppc/5343176a06654392f04b0dcf163d1f1f0f65ffce/');
-
-rdf_loader_run();
-
--- See if we have any errors
--- select * from DB.DBA.load_list where ll_state <> 2;
-
-
--- re-enable auto-indexing once finished with bulk operations
+DB.DBA.TTLP_MT(gz_file_open('/scratch/clariah-sdh/converters/src/clio_converter/rdf/qbcliogdp.ttl.gz'), '', 'http://data.socialhistory.org/resource/gdppc/5343176a06654392f04b0dcf163d1f1f0f65ffce/', 480, 0, 1);
