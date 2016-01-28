56a44f3be56552f4a97f047594dc98dbeb218456
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2016-01-28 16:36:14 +0100

    Unit tests for converter initialization and simple conversion of an example QBer dataset specification.

---
+++ src/tests/__init__.py
@@ -0,0 +1,44 @@
+import unittest
+import json
+
+from converter import Converter
+
+
+class TestConversion(unittest.TestCase):
+
+    def test_intialization(self):
+        """
+        Tests basic initialization capability of converter.Converter class
+        """
+        with open('tests/qber-output-example.json') as dataset_file:
+            dataset = json.load(dataset_file)
+
+        author_profile = {
+            'email': 'john@doe.com',
+            'name': 'John Doe',
+            'id': '2938472912'
+        }
+
+        c = Converter(dataset['dataset'], author_profile)
+
+    def test_simple_conversion(self):
+        """
+        Tests simple conversion (non-parallel) capability of converter.Converter class
+        """
+        with open('tests/qber-output-example.json') as dataset_file:
+            dataset = json.load(dataset_file)
+
+        author_profile = {
+            'email': 'john@doe.com',
+            'name': 'John Doe',
+            'id': '2938472912'
+        }
+
+        c = Converter(dataset['dataset'], author_profile)
+        c.setProcesses(1)
+
+        c.convert()
+
+
+if __name__ == '__main__':
+    unittest.main()
