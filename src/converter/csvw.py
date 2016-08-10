ea90b08407d60862355b0aa82249ee65c9ef5b29
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2016-08-10 14:50:42 +0200

    Added CSVW metadata-based converter from CSV to RDF

--- src/converter/csvw.py
+++ src/converter/csvw.py
@@ -1,15 +1,19 @@
-from util import get_namespaces
 import os
 import datetime
 import csv
 import json
 import logging
+import re
+import iribaker
+import traceback
+import rfc3987
+from util import get_namespaces, Nanopublication, QB, RDF, OWL, SKOS, XSD, SDV, SDR, PROV, namespaces
+from rdflib import Namespace, URIRef, Literal
+
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-logger.addHandler(ch)
 
 
 def build_schema(infile, outfile, delimiter=',', quotechar='\"', dataset_name=None):
@@ -17,14 +21,17 @@ def build_schema(infile, outfile, delimiter=',', quotechar='\"', dataset_name=No
     # Get the current date and time (UTC)
     today = datetime.datetime.utcnow().strftime("%Y-%m-%d")
 
+    if dataset_name is None:
+        dataset_name = url
+
     metadata = {
         "@context": ["http://www.w3.org/ns/csvw", {"@language": "en", "@base": "http://data.socialhistory.org/ns/resource/"}, get_namespaces()],
         "url": url,
         "dc:title": dataset_name,
         "dcat:keyword": [],
         "dc:publisher": {
-            "schema:name": "Example Municipality",
-            "schema:url": {"@id": "http://example.org"}
+            "schema:name": "CLARIAH Structured Data Hub - Datalegend",
+            "schema:url": {"@id": "http://datalegend.org"}
         },
         "dc:license": {"@id": "http://opendefinition.org/licenses/cc-by/"},
         "dc:modified": {"@value": today, "@type": "xsd:date"},
@@ -60,3 +67,115 @@ def build_schema(infile, outfile, delimiter=',', quotechar='\"', dataset_name=No
 
     logger.info("Done")
     return
+
+
+
+
+class CSVWConverter(object):
+
+    def __init__(self, file_name):
+
+        self.file_name = file_name
+        self.np = Nanopublication(file_name)
+
+        schema_file_name = file_name + '-metadata.json'
+        metadata = json.load(open(schema_file_name,'r'))
+
+        self.base = metadata['@context'][1]['@base']
+        self.BASE = Namespace(self.base)
+        self.schema = metadata['tableSchema']
+
+        namespaces.update({ns: url for ns, url in metadata['@context'][1].items() if not ns.startswith('@')})
+
+        self.aboutURLSchema = self.schema['aboutUrl']
+        self.columns = self.schema['columns']
+
+
+    def convert(self):
+        logger.info("Starting conversion")
+        with open(self.file_name) as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                for c in self.columns:
+                    # default
+                    s = self.expandURL(self.aboutURLSchema, row)
+
+                    try:
+                        if 'valueUrl' in c:
+                            # This is an object property
+                            if 'virtual' in c and c['virtual'] and 'aboutUrl' in c:
+                                s = self.expandURL(c['aboutUrl'], row)
+
+                            p = self.expandURL(c['propertyUrl'], row)
+                            o = self.expandURL(c['valueUrl'], row)
+                        else:
+                            # This is a datatype property
+
+                            value = row[c['name']].decode('latin')
+                            if len(value) == 0:
+                                # Skip value if length is zero
+                                continue
+
+                            # If propertyUrl is specified, use it, otherwise use the column name
+                            if 'propertyUrl' in c:
+                                p = self.expandURL(c['propertyUrl'], row)
+                            else :
+                                p = self.expandURL(c['name'], row)
+
+                            if 'datatype' in c:
+                                if c['datatype'] == 'string' and 'lang' in c:
+                                    # If it is a string datatype that has a language, we turn it into a language tagged literal
+                                    o = Literal(value, lang=c['lang'])
+                                elif isinstance(c['datatype'], dict) :
+                                    # If it is a restricted datatype, we only use its base
+                                    dt = self.expandURL(c['datatype']['base'], row, datatype=True)
+                                    o = Literal(value, datatype=dt)
+                                else:
+                                    # Otherwise we just use the datatype specified
+                                    dt = self.expandURL(c['datatype'], row, datatype=True)
+                                    o = Literal(value, datatype=dt)
+                            else:
+                                # It's just a plain literal without datatype.
+                                o = Literal(value)
+
+                        # Add the triple to the assertion graph
+                        self.np.ag.add((s, p, o))
+                    except Exception as e:
+                        print row['auto_id'], value
+                        traceback.print_exc()
+        logger.info("... done")
+
+    def serialize(self):
+        trig_file_name = self.file_name + '.trig'
+        logger.info("Starting serialization to {}".format(trig_file_name))
+
+        with open(trig_file_name, 'w') as f:
+            self.np.serialize(f, format='trig')
+        logger.info("... done")
+
+    def expandURL(self, url, row, datatype=False):
+        url = url.format(**row)
+        for ns, nsuri in namespaces.items():
+            if url.startswith(ns):
+                url = url.replace(ns + ':', nsuri)
+                break
+
+        try:
+            rfc3987.parse(url, rule='IRI')
+            iri = iribaker.to_iri(url)
+        except:
+            try:
+                if datatype==False:
+                    fullurl = self.base + url
+                else:
+                    # TODO: This should include the custom namespaces as defined in the CSVW spec
+                    fullurl = namespaces['xsd'] + url
+
+                rfc3987.parse(fullurl, rule='IRI')
+                iri = iribaker.to_iri(fullurl)
+            except:
+                raise Exception("Cannot convert `{}` to valid IRI".format(url))
+
+        # print "Baked: ", iri
+        return URIRef(iri)
---
+++ src/csvw-tool.py
@@ -0,0 +1,27 @@
+from converter.csvw import CSVWConverter, build_schema
+import argparse
+
+parser = argparse.ArgumentParser(description="Not nearly CSVW compliant schema builder and RDF converter")
+parser.add_argument('mode', choices=['convert','build'], default='convert', help='Use the schema of the `file` specified to convert it to RDF, or build a schema from scratch.')
+parser.add_argument('file', type=str, help="Path of the file that should be used for building or converting. Must be a CSV file.")
+parser.add_argument('--dataset', dest='dataset', type=str, help="A short name (slug) for the name of the dataset (will use input file name if not specified)")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if args.mode == 'build':
+        source_file = args.file
+        target_file = "{}-metadata.json".format(args.file)
+        build_schema(source_file, target_file, dataset_name=args.dataset)
+
+    elif args.mode == 'convert':
+        source_file = args.file
+
+        c = CSVWConverter(source_file)
+        c.convert()
+        c.serialize()
+    else :
+        print "Whoops"
+
+# FILE = '../sdh-private-hisco-datasets/hisco_45.csv'
+# SCHEMA = '../sdh-private-hisco-datasets/hisco_45.csv-metadata.json'
