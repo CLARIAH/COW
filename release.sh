42c17a941a8e986c1bf1cd33a2826e19b5b8291b
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2019-11-22 14:47:10 +0100

    id

--- release.sh
+++ release.sh
@@ -2,13 +2,29 @@
 
 # Releases COW to pypi via specified tag
 
+echo 'Publishing version '$1' ...'
+sleep 2
+echo 'Uploading tags...'
 git tag $1 -m "$1"
 git push --tags origin master
+sleep 2
+echo 'Renaming version...'
 sed -i "s/x.xx/$1/" setup.py src/csvw_tool.py
-rm -rf dist/
+sleep 2
+echo 'Cleaning previous dist content...'
+rm -rf dist/ local/
+sleep 2
+echo 'Preparing dist...'
 python setup.py sdist
+sleep 2
+echo 'Uploading to pipy...'
 twine upload dist/*
-rm -rf dist/
+rm -rf dist/ local/
+sleep 2
+echo 'Renaming back to version generic...'
 sed -i "s/$1/x.xx/" setup.py src/csvw_tool.py
-sleep 5
+echo 'Upgrading user lib version...'
+sleep 10
 pip install cow_csvw --upgrade
+echo 'All done.'
+exit 0
