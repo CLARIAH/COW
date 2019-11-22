dae452bbb05738dcbbac349558058b7db80a6ae2
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2019-11-22 14:27:28 +0100

    dist cleanup before twine upload

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@
 git tag $1 -m "$1"
 git push --tags origin master
 sed -i "s/x.xx/$1/" setup.py src/csvw_tool.py
+rm -rf dist/
 python setup.py sdist
 twine upload dist/*
 sudo pip install cow_csvw --upgrade
