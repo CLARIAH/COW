03137c895c89343b44bc06de7872275214ee3ae1
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2019-11-22 14:32:25 +0100

    automatic release

--- release.sh
+++ release.sh
@@ -8,5 +8,7 @@ sed -i "s/x.xx/$1/" setup.py src/csvw_tool.py
 rm -rf dist/
 python setup.py sdist
 twine upload dist/*
-pip install cow_csvw --upgrade
+rm -rf dist/
 sed -i "s/$1/x.xx/" setup.py src/csvw_tool.py
+sleep 5
+pip install cow_csvw --upgrade
