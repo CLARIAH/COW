0692ad38037ad7c4da205ad92cb664fa388fa383
Melvin Roest <melvinroest@gmail.com>
2020-10-09 11:31:00 +0200

    changed files to match release

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ echo 'Cleaning previous dist content...'
 rm -rf dist/ local/
 sleep 2
 echo 'Preparing dist...'
-python setup.py sdist
+python3 setup.py sdist
 sleep 2
 echo 'Uploading to pipy...'
 twine upload dist/*
@@ -25,6 +25,6 @@ echo 'Renaming back to version generic...'
 sed -i "s/$1/x.xx/" setup.py src/csvw_tool.py
 echo 'Upgrading user lib version...'
 sleep 10
-pip install cow_csvw --upgrade
+pip3 install cow_csvw --upgrade
 echo 'All done.'
 exit 0
